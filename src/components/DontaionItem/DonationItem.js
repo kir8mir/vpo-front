f17fed3f0e667ffcb2c4725a81efe7ec7b504f21
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-06-11 02:24:29 +0300

    ct

--- src/components/DontaionItem/DonationItem.js
+++ src/components/DontaionItem/DonationItem.js
@@ -1,10 +1,10 @@
 import { Stack, Typography } from "@mui/material";
 
 export default function DonationItem({ donation }) {
-  const { title, description } = donation;
+  const { title, description, created_at } = donation;
   return (
     <Stack>
-      <Typography variant="h4"> {title} {description}</Typography>
+      <Typography variant="h4"> {title} {created_at}</Typography>
     </Stack>
   );
 }
